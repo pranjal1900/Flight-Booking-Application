488df06a28aba29d34975fb96ed95c8092035c4b
pranjal Sharma <psharma7_be23@thapar.edu>
2026-03-13 09:30:00 +0530

    feat(frontend): setup Vite React app baseline and App routing

---
+++ frontend/src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { AuthContextProvider } from "./context/authContext.jsx";
+import { GoogleOAuthProvider } from "@react-oauth/google"
+const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <ToastContainer />
+    <AuthContextProvider>
+      <GoogleOAuthProvider clientId={CLIENT_ID}>
+      <App />
+      </GoogleOAuthProvider>
+    </AuthContextProvider>
+  </>
+);
